04191905b3d1081a73dbfe980e30cd1204069ec3
Shorton88 <shorton88@gmail.com>
2022-08-09 21:36:38 -0500

    added support for checking out of karma for rcon
    If bot finds it's out of karma it will stop the modules that require rcon for 5 minutes to let karma replenish

--- Modules/killlog.py
+++ Modules/killlog.py
@@ -40,11 +40,24 @@ def kill_stream():
     print("Exiled Kill Stream Started")
     while True:
         # read in last event time
-        connect_mariadb()
-        mariaCur.execute(
+        try:
+                dbCon = mariadb.connect(
+                user=config.DB_user,
+                password=config.DB_pass,
+                host=config.DB_host,
+                port=config.DB_port,
+                database=config.DB_name
+
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+
+        dbCur = dbCon.cursor()
+        dbCur.execute(
             "SELECT databaseLocation, Killlog_Last_Event_Time, serverName FROM servers WHERE serverName = ?",
             (config.Server_Name,))
-        server_info = mariaCur.fetchone()
+        server_info = dbCur.fetchone()
         if len(server_info) != 0 and server_info[0] is not None:
 
             gamedb_file = server_info[0]
@@ -71,8 +84,7 @@ def kill_stream():
                     ) 
                 USING (x, y, z) 
                 WHERE datetime(worldTime,'unixepoch') >= ? group by x ,y ,z 
-                ORDER BY worldTime ASC 
-                LIMIT 20
+                ORDER BY worldTime ASC
                 """,
                 (Killlog_Last_Event_Time,))
 
@@ -134,9 +146,9 @@ def kill_stream():
 
                     ##get protected areas
                     try:
-                        mariaCur.execute("SELECT name, minX, minY, maxX, maxY FROM {servername}_protected_areas".format(
+                        dbCur.execute("SELECT name, minX, minY, maxX, maxY FROM {servername}_protected_areas".format(
                             servername=serverid))
-                        protectedareas = mariaCur.fetchall()
+                        protectedareas = dbCur.fetchall()
                         if len(protectedareas) != 0 and protectedareas[0] != None:
                             for area in protectedareas:
                                 areaName = area[0]
@@ -148,25 +160,25 @@ def kill_stream():
                                 if ((int(KillLocationX) <= int(maxX)) and (int(KillLocationX) >= int(minX)) and (
                                         int(KillLocationY) <= int(maxY)) and (int(KillLocationY) >= int(minY))):
                                     try:
-                                        mariaCur.execute(
+                                        dbCur.execute(
                                             "INSERT INTO {servername}_offenders (player, platformID, current_strikes, last_strike, strike_outs) VALUES (?,?,1,?,0)".format(
                                                 servername=serverid),
                                             (player, PlayerPlatformID[0], Killlog_Last_Event_Time))
-                                        mariaCon.commit()
+                                        dbCon.commit()
                                     except Exception as e:
                                         if "Duplicate" in str(e):
-                                            mariaCur.execute(
+                                            dbCur.execute(
                                                 "SELECT current_strikes, strike_outs FROM {servername}_offenders WHERE PlatformID =?".format(
                                                     servername=serverid), (PlayerPlatformID[0],))
-                                            info = mariaCur.fetchone()
+                                            info = dbCur.fetchone()
                                             strikes = info[0]
                                             offenses = info[1]
                                             newStrikes = int(strikes) + 1
-                                            mariaCur.execute(
+                                            dbCur.execute(
                                                 "UPDATE {servername}_offenders SET current_strikes =?, last_strike =? WHERE PlatformID =?".format(
                                                     servername=serverid),
                                                 (newStrikes, Killlog_Last_Event_Time, PlayerPlatformID[0]))
-                                            mariaCon.commit()
+                                            dbCon.commit()
                                             pass
                                         else:
                                             print(e)
@@ -189,10 +201,10 @@ def kill_stream():
                     else:
                         print("checking kill streaks")
                         matchFound = 0
-                        mariaCur.execute(
+                        dbCur.execute(
                             "SELECT id, platformid, player, killstreak, highestkillstreak, wantedLevel, bounty FROM {servername}_wanted_players".format(
                                 servername=serverid))
-                        wanted = mariaCur.fetchall()
+                        wanted = dbCur.fetchall()
                         rowcount = len(wanted)
                         if rowcount != 0:
                             print("existing Wanted players found")
@@ -206,7 +218,7 @@ def kill_stream():
                                 bounty = result[6]
                                 if conanplatformid == PlayerPlatformID[0]:
                                     newkillstreak = int(killstreak) + 1
-                                    mariaCur.execute(
+                                    dbCur.execute(
                                         "UPDATE {servername}_wanted_players SET killstreak =? WHERE id =?".format(
                                             servername=serverid), (newkillstreak, playerid))
 
@@ -228,103 +240,103 @@ def kill_stream():
                                         print("something went wrong with kill streak calculation")
                                     newbounty = newwantedlevel * 1000
                                     if newkillstreak > highestkillstreak:
-                                        mariaCur.execute(
+                                        dbCur.execute(
                                             "UPDATE {servername}_wanted_players SET highestkillstreak = ? WHERE id =?".format(
                                                 servername=serverid), (newkillstreak, playerid))
-                                    mariaCur.execute(
+                                    dbCur.execute(
                                         "UPDATE {servername}_wanted_players SET wantedLevel =?, bounty =?, X =?, Y =? WHERE id =?".format(
                                             servername=serverid),
                                         (newwantedlevel, newbounty, KillLocationX, KillLocationY, playerid))
-                                    # mariaCur.execute("UPDATE {servername}_wanted_players SET bounty =? WHERE id =?".format(servername = serverid),(newbounty, playerid))
+                                    
                                     # add coin for the kill
                                     if wantedLevel > 0:
-                                        mariaCur.execute(
+                                        dbCur.execute(
                                             "SELECT walletbalance, earnratemultiplier FROM accounts WHERE conanplatformid =?",
                                             (PlayerPlatformID[0],))
-                                        balance = mariaCur.fetchone()
+                                        balance = dbCur.fetchone()
                                         if balance != None:
                                             wantedKill = True
                                             newbalance = int(balance[0]) + newbounty
-                                            mariaCur.execute(
+                                            dbCur.execute(
                                                 "UPDATE accounts SET walletbalance =? WHERE conanplatformid =?",
                                                 (newbalance, PlayerPlatformID[0]))
-                                            mariaCon.commit()
+                                            dbCon.commit()
                                             balance = balance[0]
                                             wanted_paid_amount = (200 * earnratemultiplier)
                                             newBalance = balance + wanted_paid_amount
-                                            mariaCur.execute(
+                                            dbCur.execute(
                                                 "UPDATE accounts SET walletBalance =? WHERE conanplatformid =?",
                                                 (newBalance, PlayerPlatformID[0]))
-                                            mariaCon.commit()
+                                            dbCon.commit()
                                     matchFound = 1
                                 if conanplatformid == VictimPlatformID[0]:
                                     # pay killer
-                                    mariaCur.execute(
+                                    dbCur.execute(
                                         "SELECT walletBalance, earnratemultiplier FROM accounts WHERE conanplatformid = ?",
                                         (PlayerPlatformID[0],))
-                                    walletDetails = mariaCur.fetchone()
+                                    walletDetails = dbCur.fetchone()
                                     if walletDetails != None:
-                                        bountykill = True
+                                        bountyKill = True
                                         walletBalance = walletDetails[0]
                                         earnratemultiplier = walletDetails[1]
                                         bounty_paid_amount = (bounty * earnratemultiplier)
                                         newBalance = walletBalance + bounty_paid_amount
 
-                                        mariaCur.execute(
+                                        dbCur.execute(
                                             "UPDATE accounts SET walletBalance=? WHERE conanplatformid = ?",
                                             (newBalance, PlayerPlatformID[0]))
-                                        mariaCon.commit()
+                                        dbCon.commit()
                                         # clear wanted
-                                        mariaCur.execute(
+                                        dbCur.execute(
                                             "UPDATE {servername}_wanted_players SET wantedLevel = '0' WHERE id =?".format(
                                                 servername=serverid), (playerid,))
-                                        mariaCur.execute(
+                                        dbCur.execute(
                                             "UPDATE {servername}_wanted_players SET bounty = '0' WHERE id =?".format(
                                                 servername=serverid), (playerid,))
-                                        mariaCur.execute(
+                                        dbCur.execute(
                                             "UPDATE {servername}_wanted_players SET killstreak = '0' WHERE id =?".format(
                                                 servername=serverid), (playerid,))
-                                        mariaCon.commit()
+                                        dbCon.commit()
                                         matchFound = 1
                             if matchFound == 0:
                                 print("no existing wanted players found")
-                                mariaCur.execute(
+                                dbCur.execute(
                                     "INSERT INTO {servername}_wanted_players (platformid, player, killstreak, highestkillstreak, wantedlevel, bounty, X, Y) VALUES (?,?,'1','1','0','0',?,?)".format(
                                         servername=serverid),
                                     (PlayerPlatformID[0], player, KillLocationX, KillLocationY))
-                                mariaCon.commit()
+                                dbCon.commit()
                         else:
                             print("no existing wanted players found")
-                            mariaCur.execute(
+                            dbCur.execute(
                                 "INSERT INTO {servername}_wanted_players (platformid, player, killstreak, highestkillstreak, wantedlevel, bounty, X, Y) VALUES (?,?,'1','1','0','0',?,?)".format(
                                     servername=serverid), (PlayerPlatformID[0], player, KillLocationX, KillLocationY))
-                            mariaCon.commit()
+                            dbCon.commit()
                     # insert into recent pvp
                     now = datetime.now()
                     pvpmarkername = f"{player} vs {victim}"
-                    mariaCur.execute(
+                    dbCur.execute(
                         "INSERT INTO {servername}_recent_pvp (name, x, y, loaddate) VALUES (?,?,?,?)".format(
                             servername=serverid), (pvpmarkername, KillLocationX, KillLocationY, now))
-                    mariaCon.commit()
+                    dbCon.commit()
 
                     # record last event time to db
-                    mariaCur.execute(
+                    dbCur.execute(
                         "INSERT INTO {servername}_kill_log (player, player_id, player_level, player_clan, victim, victim_id, victim_level, victim_clan, kill_location_x, kill_location_y, kill_type, protected_area, wanted_kill, wanted_paid_amount, bounty_kill, bounty_paid_amount, Killlog_Last_Event_Time) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)".format(
                             servername=serverid), (
                             player, playerID, playerLevel, playerClan, victim, victimID, victimLevel, victimClan,
                             KillLocationX, KillLocationY, killtype, ProtectedArea, wantedKill, wanted_paid_amount,
                             bountyKill, bounty_paid_amount, Killlog_Last_Event_Time))
                     # update server's last event time
-                    mariaCur.execute("UPDATE servers SET Killlog_Last_Event_Time = ? WHERE serverName = ?",
+                    dbCur.execute("UPDATE servers SET Killlog_Last_Event_Time = ? WHERE serverName = ?",
                                      (Killlog_Last_Event_Time, serverid))
-                    mariaCon.commit()
+                    dbCon.commit()
 
             # close gamedb
 
             exiled_gamedb_con.close()
 
             killList = ''
-
-        close_mariaDB()
+        dbCur.close()
+        dbCon.close()
 
         sleep(1)  # task runs every 1 seconds
--- PrimaryBot/IGBot.py
+++ PrimaryBot/IGBot.py
@@ -1,14 +1,17 @@
 from asyncio import subprocess
 from concurrent.futures import Executor, process
+from pickle import TRUE
 import time
 import concurrent.futures
 import sys
 import subprocess
 import multiprocessing
+import valve.rcon
 
 
 # add Modules folder to system path
 sys.path.insert(0, '..\\Modules')
+import config
 from killlog import kill_stream
 from discordhandler import discord_bot
 from usersync import runSync
@@ -21,6 +24,7 @@ if __name__ == "__main__":
     p = subprocess.call(["python", "..\\Setup\\setup.py"], stdout=sys.stdout)
     
     #establish bot module status variables
+    outOfKarma = False
     killlogRunning = False
     discordhandlerRunning = False
     userSyncRunning = False
@@ -28,79 +32,109 @@ if __name__ == "__main__":
     accountPayrollRunning = False
     #start modules and loop to check for status    
     while True:
-        try:
-            if not killlogRunning:
-                killlog = multiprocessing.Process(target=kill_stream)
-                killlog.start()
-                killlogRunning = True
-            if killlog.is_alive():
-                print(f'Status: Killlog is still running')
-                killlogRunning = True
-            else:
-                print(f'Status: Killlog is not running. Restarting...')
+        if outOfKarma == False:
+            try:
+                if not killlogRunning:
+                    killlog = multiprocessing.Process(target=kill_stream)
+                    killlog.start()
+                    killlogRunning = True
+                if killlog.is_alive():
+                    print(f'Status: Killlog is still running')
+                    killlogRunning = True
+                else:
+                    print(f'Status: Killlog is not running. Restarting...')
+                    killlogRunning = False
+            except Exception as e:
+                print(f'Kill Log Error: {e}')
                 killlogRunning = False
-        except Exception as e:
-            print(f'Kill Log Error: {e}')
-            killlogRunning = False
-        
-        try:
-            if not discordhandlerRunning:
-                discordhandler = multiprocessing.Process(target=discord_bot)
-                discordhandler.start()
-                discordhandlerRunning = True
-            if discordhandler.is_alive():
-                print(f'Status: Discord Handler is still running')
-                discordhandlerRunning = True
-            else:
-                print(f'Status: Discord Handler is not running. Restarting...')
+            
+            try:
+                if not discordhandlerRunning:
+                    discordhandler = multiprocessing.Process(target=discord_bot)
+                    discordhandler.start()
+                    discordhandlerRunning = True
+                if discordhandler.is_alive():
+                    print(f'Status: Discord Handler is still running')
+                    discordhandlerRunning = True
+                else:
+                    print(f'Status: Discord Handler is not running. Restarting...')
+                    discordhandlerRunning = False
+            except Exception as e:
+                print(f'Discord Handler Error: {e}')
                 discordhandlerRunning = False
-        except Exception as e:
-            print(f'Discord Handler Error: {e}')
-            discordhandlerRunning = False
 
-        try:
-            if not userSyncRunning:
-                userSync = multiprocessing.Process(target=runSync, args=(False,))
-                userSync.start()
-                userSyncRunning = True
-            if userSync.is_alive():
-                print(f'Status: User Sync is still running')
-                userSyncRunning = True
-            else:
-                print(f'Status: User Sync is not running. Restarting...')
+            try:
+                if not userSyncRunning:
+                    userSync = multiprocessing.Process(target=runSync, args=(False,))
+                    userSync.start()
+                    userSyncRunning = True
+                if userSync.is_alive():
+                    print(f'Status: User Sync is still running')
+                    userSyncRunning = True
+                else:
+                    print(f'Status: User Sync is not running. Restarting...')
+                    userSyncRunning = False
+            except Exception as e:
+                print(f'User Sync Error: {e}')
                 userSyncRunning = False
-        except Exception as e:
-            print(f'User Sync Error: {e}')
-            userSyncRunning = False
 
-        try:
-            if not gameLogWatcherRunning:
-                gameLogWatcher = multiprocessing.Process(target=game_log_watcher)
-                gameLogWatcher.start()
-                gameLogWatcherRunning = True
-            if gameLogWatcher.is_alive():
-                print(f'Status: Game Log Watcher is still running')
-                gameLogWatcherRunning = True
-            else:
-                print(f'Status: Game Log Watcher is not running. Restarting...')
+            try:
+                if not gameLogWatcherRunning:
+                    gameLogWatcher = multiprocessing.Process(target=game_log_watcher)
+                    gameLogWatcher.start()
+                    gameLogWatcherRunning = True
+                if gameLogWatcher.is_alive():
+                    print(f'Status: Game Log Watcher is still running')
+                    gameLogWatcherRunning = True
+                else:
+                    print(f'Status: Game Log Watcher is not running. Restarting...')
+                    gameLogWatcherRunning = False
+            except Exception as e:
+                print(f'Game Log Watcher error: {e}')
                 gameLogWatcherRunning = False
-        except Exception as e:
-            print(f'Game Log Watcher error: {e}')
-            gameLogWatcherRunning = False
 
-        try:
-            if not accountPayrollRunning:
-                accountPayroll = multiprocessing.Process(target=pay_users)
-                accountPayroll.start()
-                accountPayrollRunning = True
-            if accountPayroll.is_alive():
-                print(f'Status: Account Payroll is still running')
-                accountPayrollRunning = True
-            else:
-                print(f'Status: Account Payroll is not running. Restarting...')
+            try:
+                if not accountPayrollRunning:
+                    accountPayroll = multiprocessing.Process(target=pay_users)
+                    accountPayroll.start()
+                    accountPayrollRunning = True
+                if accountPayroll.is_alive():
+                    print(f'Status: Account Payroll is still running')
+                    accountPayrollRunning = True
+                else:
+                    print(f'Status: Account Payroll is not running. Restarting...')
+                    accountPayrollRunning = False
+            except Exception as e:
+                print(f'Account Payroll error: {e}')
                 accountPayrollRunning = False
-        except Exception as e:
-            print(f'Account Payroll error: {e}')
-            accountPayrollRunning = False
 
-        time.sleep(10) # wait 10 seconds before checking status
+            attempts = 0
+            rconSuccess = False
+            while rconSuccess == False and attempts <= 5:
+                try:
+                    with valve.rcon.RCON((config.Server_RCON_Host, int(config.Server_RCON_Port)), config.Server_RCON_Pass) as rcon:
+                        response = rcon.execute("listplayers")
+                        rcon.close()
+                        response_text = response.body.decode('utf-8', 'ignore')
+                    rconSuccess = True
+                except valve.rcon.RCONAuthenticationError:
+                    print("RCON watcher error: Authentication Error")
+                    status = "Could not authenticate RCON"
+                    rconSuccess = True
+                    attempts = attempts + 1
+                    pass
+                except ConnectionResetError:
+                    print("RCON watcher error: Could not connect to server. Retry later")
+                    status = "Could not connect to server, possibly out of karma"
+                    rconSuccess = 0
+                    attempts = attempts + 5
+                    outOfKarma = True
+                    pass
+
+            time.sleep(30) # wait 30 seconds before checking status
+        else:
+            print("Out of Karma! Stopping all rcon dependant modules until karma is regained...")
+            userSync.terminate()
+            userSyncRunning = False
+            time.sleep(300) # wait 5 minutes before releasing modules
+            outOfKarma = False
